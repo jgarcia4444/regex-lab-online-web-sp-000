ec89485e6aba7d5e592782459110c02b35603027
Jake Garcia <bballwiz4@gmail.com>
2020-02-20 20:21:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,5 +22,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/[0-9]{10}/)
+  if phone.match(/[0-9]{10}/) != nil
+    return true
+  end
+  return false
 end
