0a46576863539bae0ae5e94d3240354e6c44a496
Jake Garcia <bballwiz4@gmail.com>
2020-02-20 19:52:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,6 +17,8 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/[A-Z]+\w+./)
     return true
+  elsif text.match(/[a-z]+\w+./)
+    return false
   else
     return false
   end
